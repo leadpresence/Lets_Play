364215cbb2ca7070298b672055f87927a81caa47
ChiFelix <felix.chibueze@wayapaychat.com>
2022-04-22 00:18:56 +0100

    Fixed list view not scrolling

--- lib/app/config/routes/app_pages.dart
+++ lib/app/config/routes/app_pages.dart
@@ -35,7 +35,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const initial = Routes.walletHome;
+  static const initial = Routes.splash;
 
   static final routes = [
     GetPage(
